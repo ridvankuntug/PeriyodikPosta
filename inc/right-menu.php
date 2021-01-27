55932bf037e597c5b301b51da6506f110a07b36e
Rıdvan KÜNTUĞ <50447691+ridvankuntug@users.noreply.github.com>
2021-01-27 21:21:07 +0300

    Update right-menu.php

--- inc/right-menu.php
+++ inc/right-menu.php
@@ -1,6 +1,6 @@
     <div class="col-sm-3"><!--Sayfanın ne kadarını kaplayacağını belirliyoruz-->
       <h3>Hakkımızda</h3>
-      <p>ISG HARRAN Periyodik Mail Sistemi</p>
+      <p>Periyodik Mail Sistemi</p>
       <h3>Menü</h3>
       <ul class="nav nav-pills flex-column">
         <?php require("inc/links.php"); ?><!--Menü içeriğimiz links.php sayfasından geliyor-->
